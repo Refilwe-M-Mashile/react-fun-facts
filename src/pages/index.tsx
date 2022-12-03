91e746c80437fa94d864b1b79ccdf2f18b60e0df
Refilwe-M-Mashile <=>
2022-12-04 01:09:56 +0200

    initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Index = () => {
+  return (
+    <>
+      <a href="https://reactjs.org" target="_blank">
+        <img
+          src="src/assets/react.svg"
+          className="logo react"
+          alt="React logo"
+          width="40px"
+        />
+      </a>
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created by Jordan Walke</li>
+        <li>Has well over 100K stars on GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  );
+};
+
+export default Index;
